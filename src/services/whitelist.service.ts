79fb759571e997787bcc99cd8af0ff4e33cf0708
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-04-07 22:38:49 +0100

    get the whitelist service done

--- src/services/whitelist.service.ts
+++ src/services/whitelist.service.ts
@@ -1,7 +1,7 @@
 import { normalizeEmail } from "../utils/validators";
 import { normalizeIp } from "../utils/ipHelper";
 import { ServiceResult, WhitelistDTO } from "../types";
-import prisma from "../config/databse";
+import { prisma } from "../config/databse";
 
 /**
  * Service class for whitelist operations
