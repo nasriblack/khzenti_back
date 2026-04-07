c4362f7cba351aeb69416cc28be04813abd62c45
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-04-07 22:37:17 +0100

    update in database and fix the bug(

--- src/config/databse.ts
+++ src/config/databse.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const connectionString = process.env.DATABASE_URL;
 
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-export default prisma;
+export { prisma };
